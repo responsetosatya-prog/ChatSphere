b69c7f80020d00a2125533de3f0f9b1dce042c17
responsetosatya-prog <responsetosatya@gmail.com>
2026-06-28 01:38:41 +0530

    Create generateToken.js

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+/*
+==========================================
+Generate JWT Token
+==========================================
+*/
+
+export function generateToken(user) {
+
+    return jwt.sign(
+
+        {
+            id: user.id,
+            email: user.email,
+            role: user.role
+        },
+
+        process.env.JWT_SECRET,
+
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN || "7d"
+        }
+
+    );
+
+}
