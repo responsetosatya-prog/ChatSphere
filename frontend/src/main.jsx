5a0430576d20726fef02d1493e5aa6c6d135488f
responsetosatya-prog <responsetosatya@gmail.com>
2026-06-28 10:06:57 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
-// frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./index.css";  // ← This imports all styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
